4cd15013d7747d38e640ff504412c5fd8626e371
milosz08 <gilgamilosz451@gmail.com>
2023-08-14 01:43:17 +0200

    update footer tailwind styles

--- tailwind/ui/footer.cjs
+++ tailwind/ui/footer.cjs
@@ -28,13 +28,20 @@
 const { parseTwdApply } = require('../utils.cjs');
 
 module.exports = {
-  '.msph_footer-container': parseTwdApply([
+  '.msph_footer__container': parseTwdApply([
     'p-5 md:p-5 lg:p-10',
     'border-t-2 border-gray-300 dark:border-gray-700',
     'bg-msph-primary-light dark:bg-msph-primary-dark',
     'text-msph-primary-dark dark:text-msph-primary-light',
   ]),
-  '.msph_footer-select-list-container': parseTwdApply([
+  '.msph_footer__select-list-button': parseTwdApply([
+    'border border-gray-500',
+    'px-2.5 py-1.5',
+    'rounded-md',
+    'flex items-center',
+    'font-medium dark:font-normal',
+  ]),
+  '.msph_footer__select-list-container': parseTwdApply([
     'absolute',
     'left-0 bottom-full',
     'w-max max-h-[200px]',
@@ -44,20 +51,43 @@ module.exports = {
     'flex flex-col gap-1',
     'overflow-y-auto',
     'font-medium dark:font-normal',
-    'bg-msph-primary-light dark:bg-msph-primary-dark',
   ]),
-  '.msph_footer-select-list-element': parseTwdApply([
+  '.msph_footer__select-list-element': parseTwdApply([
     'flex items-center',
     'rounded-md',
     'w-full',
     'cursor-pointer',
     'p-1 pb-1.5 px-2',
-    'hover:bg-gray-300 dark:hover:bg-gray-600',
   ]),
-  '.msph_footer-select-list-element-active': parseTwdApply([
+  '.msph_footer__select-list-element--active': parseTwdApply([
     'bg-msph-primary-tint',
     'text-msph-primary-light',
     'pointer-events-none',
     'hover:text-msph-primary-dark dark:hover:bg-gray-600',
+  ]),
+  '.msph_footer__link': parseTwdApply([
+    'hover:underline',
+    'block',
+    'mb-1',
+    'dark:font-light',
+    'leading-[22px]',
+  ]),
+  '.msph_copy-footer__container': parseTwdApply([
+    'flex flex-col md:flex-row',
+    'justify-center md:justify-between items-center',
+    'gap-y-5',
+  ]),
+  '.msph_copy-footer__rights': parseTwdApply([
+    'flex flex-col md:flex-row',
+    'gap-y-2 md:gap-y-0 gap-x-7',
+    'text-sm',
+    'font-light',
+  ]),
+  '.msph_copy-footer__open-app-button': parseTwdApply([
+    'msph_small-button',
+    'bg-msph-primary-tint',
+    'text-msph-primary-light',
+    'h-fit w-full md:w-fit',
+    'text-center',
   ]),
 };
