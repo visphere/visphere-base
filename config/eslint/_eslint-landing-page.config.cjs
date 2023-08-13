2acbdc6302a8fd81414068082c6c94c967a1f086
milosz08 <gilgamilosz451@gmail.com>
2023-08-13 05:39:59 +0200

    create eslint and prettier config files for landing-page

---
+++ config/eslint/_eslint-landing-page.config.cjs
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2023 by MILOSZ GILGA <https://miloszgilga.pl>
+ * Silesian University of Technology
+ *
+ *   File name: _eslint-landing-page.config.cjs
+ *   Created at: 2023-08-11, 18:35:47
+ *   Last updated at: 2023-08-11, 20:37:41
+ *
+ *   Project name: moonsphere
+ *   Module name: moonsphere-base
+ *
+ * This project is a part of "MoonSphere" instant messenger system. This system is a part of
+ * completing an engineers degree in computer science at Silesian University of Technology.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain a copy of the License at
+ *
+ *   <http://www.apache.org/license/LICENSE-2.0>
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the license.
+ */
+
+'use strict';
+
+module.exports = {
+  root: true,
+  ignorePatterns: ['projects/**/*'],
+  extends: ['plugin:astro/recommended', 'plugin:json/recommended'],
+  overrides: [
+    {
+      files: ['*.astro'],
+      plugins: ['astro'],
+      env: {
+        node: true,
+        'astro/astro': true,
+        es2020: true,
+      },
+      parser: 'astro-eslint-parser',
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+        extraFileExtensions: ['.astro'],
+        sourceType: 'module',
+      },
+      rules: {
+        'astro/no-conflict-set-directives': 'error',
+        'astro/no-unused-define-vars-in-style': 'error',
+      },
+    },
+    {
+      files: ['*.ts'],
+      extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+      rules: {
+        '@typescript-eslint/no-non-null-assertion': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/semi': ['error', 'always'],
+      },
+    },
+  ],
+};
---
+++ config/prettier/_prettier-landing-page.config.cjs
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2023 by MILOSZ GILGA <https://miloszgilga.pl>
+ * Silesian University of Technology
+ *
+ *   File name: _prettier-landing-page.config.cjs
+ *   Created at: 2023-08-11, 18:32:49
+ *   Last updated at: 2023-08-11, 20:37:53
+ *
+ *   Project name: moonsphere
+ *   Module name: moonsphere-base
+ *
+ * This project is a part of "MoonSphere" instant messenger system. This system is a part of
+ * completing an engineers degree in computer science at Silesian University of Technology.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain a copy of the License at
+ *
+ *   <http://www.apache.org/license/LICENSE-2.0>
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the license.
+ */
+
+'use strict';
+
+const prettierBaseConfig = require('./_prettier-base.config.cjs');
+
+module.exports = {
+  ...prettierBaseConfig,
+  importOrderParserPlugins: ['typescript', 'decorators-legacy'],
+  plugins: [
+    '@trivago/prettier-plugin-sort-imports',
+    require.resolve('prettier-plugin-astro'),
+  ],
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro',
+      },
+    },
+  ],
+  importOrder: ['^(astro*)$', '^([a-zA-Z].*)$', '^~/(.*)$', '^[./]', '^[../]'],
+  importOrderSeparation: false,
+  importOrderSortSpecifiers: true,
+  astroAllowShorthand: false,
+};
